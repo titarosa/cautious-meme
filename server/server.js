ef274b8e9a0ee08d5ff32610efa47ac8a1080427
Talita Rosa de Oliveira <talitarosa.02@hotmail.com>
2024-11-03 10:52:53 -0500

    Fixing bug

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Serve static files from the client build directory
+app.use(express.static(path.join(__dirname, '../client/dist'))); // Adjust path based on your directory structure
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Import and use your HTML routes
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
